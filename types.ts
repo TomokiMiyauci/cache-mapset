23ca06ffad7f8892e0f5e381f500afd2167d3910
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-11 20:27:48 +0900

    docs(types): add JSDOC

--- types.ts
+++ types.ts
@@ -2,15 +2,21 @@
 // This module is browser compatible.
 
 export interface MapLike<K, V> {
+  /** The number of entries. */
   size: number;
 
+  /** Whether has an entry with the given {@link key}. */
   has: (key: K) => boolean;
 
+  /** Returns the value of the entry with the given {@link key}, if any such entry exists; otherwise returns `undefined`. */
   get: (key: K) => V | undefined;
 
+  /** Adds an entry with the given {@link key} mapped to the given {@link value}. */
   set: (key: K, value: V) => this;
 
+  /** Deletes the entry with the given {@link key}. */
   delete: (key: K) => boolean;
 
+  /** Removes all entries. */
   clear: () => void;
 }
