f1542efbaa817e83ebe2fad8c6c14d6f8a0556e5
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-13 01:09:04 +0900

    chore(_tools): add npm build script

---
+++ _tools/meta.ts
@@ -0,0 +1,46 @@
+import { BuildOptions } from "https://deno.land/x/dnt@0.37.0/mod.ts";
+
+export const makeOptions = (version: string): BuildOptions => ({
+  test: false,
+  shims: {},
+  typeCheck: "both",
+  entryPoints: ["./lru.ts", "./lfu.ts", "./fifo.ts", "./lifo.ts"],
+  outDir: "./npm",
+  package: {
+    name: "cache-mapset",
+    version,
+    description:
+      "Maps and Sets with cache replacement policies, TC39 proposal-policy-map-set implementation",
+    keywords: [
+      "cache",
+      "map",
+      "set",
+      "map-like",
+      "set-like",
+      "lru",
+      "lfu",
+      "fifo",
+      "tc39",
+      "proposal-policy-map-set",
+    ],
+    license: "MIT",
+    homepage: "https://github.com/TomokiMiyauci/cache-mapset",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/TomokiMiyauci/cache-mapset.git",
+    },
+    bugs: {
+      url: "https://github.com/TomokiMiyauci/cache-mapset/issues",
+    },
+    sideEffects: false,
+    type: "module",
+  },
+  packageManager: "pnpm",
+  mappings: {
+    "https://deno.land/x/isx@1.4.0/numeric/is_non_negative_number.ts": {
+      name: "@miyauci/isx",
+      version: "1.4.0",
+      subPath: "numeric/is_non_negative_number.js",
+    },
+  },
+});
